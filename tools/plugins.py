107047b6ac00172e01aede8864965d102f18c5da
Matthias Sohn <matthias.sohn@sap.com>
2020-03-14 00:05:58 +0100

    Plugins list: add plugin maintainers to generated plugin list
    This helps users to find plugin maintainers.
    
    Basic authentication credentials of a user having read access to the
    plugins owner group information can be passed via environment variables
    "user" and "password" otherwise the user is prompted for the missing
    credentials.
    
    Bug: Issue 11640
    Change-Id: Ib2a3e6515133b65063c4cd6102b3fdf0252da00a

--- tools/plugins.py
+++ tools/plugins.py
@@ -1,15 +1,28 @@
+import getpass
+import os
 import re
 import requests
 
-from pygerrit2 import GerritRestAPI
+from pygerrit2 import GerritRestAPI, HTTPBasicAuth
+
+def authenticate():
+    username = os.environ.get("user")
+    password = os.environ.get("password")
+    while not username:
+        username = input("user: ")
+    while not password: 
+        password = getpass.getpass("password: ")
+    auth = HTTPBasicAuth(username, password)
+    return auth
 
 url = "https://gerrit-review.googlesource.com"
-api = GerritRestAPI(url=url)
+api = GerritRestAPI(url=url, auth=authenticate())
+
 plugins = api.get("/projects/?p=plugins%2f&d")
 
-header = "|Name|State|Changes (last 3 months)|Description"
-dashes = "|----|-----|-------|-----------"
-spacer = "|    |     |       |           "
+header = "|Name|State|Changes (last 3 months)|Description|Maintainers"
+dashes = "|----|-----|-----------------------|-----------|---"
+spacer = "|    |     |                       |           |   "
 
 branches = ["master"] + ["stable-%s" % version for version in ["3.1", "3.0", "2.16"]]
 
@@ -57,6 +70,19 @@ def getRecentChangesCount(pluginName):
     return len(changes)
 
 
+def getOwnerNames(pluginName):
+    groups = api.get("/groups/?query=name:plugins-%s" % pluginName)
+    if len(groups) > 0:
+        ownerGroup = groups[0]
+        id = ownerGroup.get("id")
+        owners = api.get("/groups/%s/members/" % id)
+        names = [o.get("name") for o in owners]
+        names = ", ".join(names)
+    else:
+        names = ""
+    return names
+
+
 page = "plugins"
 with open("tools/template.md", "r") as template:
     with open("pages/site/plugins/%s.md" % page, "w") as output:
@@ -101,11 +127,14 @@ with open("tools/template.md", "r") as template:
             else:
                 description = unicodeSquare
 
-            line = "|[%s]|%s|%d|%s|%s|\n" % (
+            owners = getOwnerNames(name)
+
+            line = "|[%s]|%s|%d|%s|%s|%s|\n" % (
                 name,
                 state,
                 changes,
                 description,
+                owners,
                 availableBranches,
             )
             output.write(line)
