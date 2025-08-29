f1d6caa13ef212c776c3be48221ca051a7717d9c
Daniele Sassoli <danielesassoli@gmail.com>
2025-08-29 17:13:16 +0100

    Update docker entrypoint
    Change-Id: Ib639d58264c6336a4585672321c03f90b33bad60

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 #cd /site
 
+# Ensure we use the newer bundler version
+export BUNDLE_GEMFILE=/site/Gemfile
 
 if [ "$JEKYLL_NEW" = true ]; then
   echo "NOTE: making new jekyll site!"
@@ -20,6 +22,7 @@ if [ ! -f Gemfile ]; then
   exit 1
 fi
 
+# Install dependencies
 bundle install
 
 exec "$@"
